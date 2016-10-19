06f34fa914422bca7c9a222313ed0df00b7fc784
Oliver Vieregge <oliverv@trakken.de>
2016-10-19 16:30:16 +0200

    typo

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-case "$0" in
+case "$1" in
     master)
         SPARK_PUBLIC_DNS=`curl -s rancher-metadata/latest/self/container/name`.rancher.internal \
         SPARK_LOCAL_IP=`curl -s rancher-metadata/latest/self/container/primary_ip` \
         MASTER=$SPARK_PUBLIC_DNS \
-        exec bin/spark-class org.apache.spark.deploy.master.Master -h $1
+        exec bin/spark-class org.apache.spark.deploy.master.Master -h $SPARK_PUBLIC_DNS
         ;;
     worker)
         SPARK_PUBLIC_DNS=`curl -s rancher-metadata/latest/self/container/name`.rancher.internal \
         SPARK_LOCAL_IP=`curl -s rancher-metadata/latest/self/container/primary_ip` \
-        exec bin/spark-class org.apache.spark.deploy.worker.Worker $1
+        exec bin/spark-class org.apache.spark.deploy.worker.Worker $2
         ;;
     bash)
         SPARK_PUBLIC_DNS=`curl -s rancher-metadata/latest/self/container/name`.rancher.internal \
